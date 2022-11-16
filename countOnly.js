9b39666f124a683923e60ec1f3c147ef279e1fae
ss-aa123 <neermas1@hotmail.com>
2022-11-16 18:03:04 -0500

    given a collection of items, return an object containing counts of everything the input object listed. Using it in conjuction with the assert equal function

---
+++ countOnly.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  let compare = actual === expected;
+  if (compare) {
+    console.log(`👽👽👽 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+
+    // increment the counter for each item:
+    // set a property with that string key to:
+    // the value that was already there (or zero if nothing there with 1 added to it.
+    if (itemsToCount[item]) {
+      results[item] ? results[item] += 1 : results[item] = 1;
+    }
+  }
+  return results;
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
