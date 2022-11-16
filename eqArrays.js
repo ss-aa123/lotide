b0f9cce38b35d7b72c5d335ab517531d98951e2a
ss-aa123 <neermas1@hotmail.com>
2022-11-15 21:15:18 -0500

    first commit. Creating a function that takes 2 arrays and returns true or false based on if they match. Then using the function with assertEqual.js function

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let compare = actual === expected;
+  if (compare) {
+    console.log(`👽👽👽 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let compare = true;
+  for (let i = 0; i <= arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      compare = false;
+    }
+  }
+  return compare;
+};
+
+//TEST CODE
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
