b547276d5271e7b777e930282dbe006d334170f7
ss-aa123 <neermas1@hotmail.com>
2022-11-18 17:43:22 -0500

    first commit

---
+++ map.js
@@ -0,0 +1,31 @@
+const eqArrays = function(arr1, arr2) {
+  let compare = true;
+  for (let i = 0; i <= arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      compare = false;
+    }
+  }
+  return compare;
+};
+const assertArraysEqual = function(arr1, arr2) {
+  let answer = eqArrays(arr1,arr2);
+  if (answer) {
+    console.log(`👽👽👽 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1,[ 'g', 'c', 't', 'm', 't' ]);
+
+
