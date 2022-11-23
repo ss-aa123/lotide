7ae3a69bbe4bc696ec8a5217a69651e5b0dacd91
ss-aa123 <neermas1@hotmail.com>
2022-11-23 01:56:38 -0500

    first commit seperating the etst code

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+const eqArrays = require('../eqArrays')
+const assertArraysEqual = require('../assertArraysEqual')
+
+//TEST CODE
+assertArraysEqual([1,2,3],[1,2,3]);
+assertArraysEqual([1,2,3],[1,2,4]);
