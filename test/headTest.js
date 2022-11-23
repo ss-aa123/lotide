d3f20fd7f864f9c93c82ae7de77952ed013b8f3b
ss-aa123 <neermas1@hotmail.com>
2022-11-23 01:37:55 -0500

    first commit

---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([3]), 5);
+assertEqual(head([3]), 3);
