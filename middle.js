50348397e3b99109ca73ee7c04c9b37e838e844c
ss-aa123 <neermas1@hotmail.com>
2022-11-23 02:30:02 -0500

    origin master

--- middle.js
+++ middle.js
@@ -1,21 +1,6 @@
-const eqArrays = function(arr1, arr2) {
-  let compare = true;
-  for (let i = 0; i <= arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      compare = false;
-    }
-  }
-  return compare;
-};
+const eqArrays = require('./eqArrays'); 
+const assertArraysEqual = require('./assertArraysEqual'); 
 
-const assertArraysEqual = function(arr1, arr2) {
-  let answer = eqArrays(arr1,arr2);
-  if (answer) {
-    console.log(`👽👽👽 Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`😡😡😡 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
 
 const middle = function(arr) {
   let mid = [];
@@ -33,20 +18,4 @@ const middle = function(arr) {
   return mid;
 };
 
-// TEST CODE
-console.log(
-  middle([1]), // => []
-  middle([1, 2]), // => []
-
-  middle([1, 2, 3]), // => [2]
-  middle([1, 2, 3, 4, 5]), // => [3]
-
-  middle([1, 2, 3, 4]), // => [2, 3]
-  middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-);
-
-// TESTING OTHER FUNCTIONS WITH THE MIDDLE FUNCTION
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
