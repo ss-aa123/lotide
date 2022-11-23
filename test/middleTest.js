5b46b6d1eaa7c5edd261d37002b367eba41ec599
ss-aa123 <neermas1@hotmail.com>
2022-11-23 09:34:57 -0500

    refactored again

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,7 @@
+ /*
  const middle = require('../middle')
  const assertArraysEqual = require('../assertArraysEqual')
  const eqArrays = require('../eqArrays'); 
-
- 
 // TEST CODE
 console.log(
   middle([1]), // => []
@@ -20,3 +19,17 @@ assertArraysEqual(middle([1, 2]), []);
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+*/
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), 2);
+  });
+
+  it("returns 2, 3 for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2,3]); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,3 +1,4 @@
+/*
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
@@ -7,3 +8,17 @@ console.log(tail(words));
 assertEqual(tail(words).length, 2);
 assertEqual(tail(words)[0],"Lighthouse");
 assertEqual(tail(words)[1],"Labs");
+*/
+
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+describe("#tail", () => {
+  it("returns 2, 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(tail(['5']), '5'); 
+  });
+});
