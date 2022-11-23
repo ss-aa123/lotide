d9ed810b59b24207ac2cfae553092fc0ac291e19
ss-aa123 <neermas1@hotmail.com>
2022-11-23 01:44:39 -0500

    refactored it and its related tailTest file

--- tail.js
+++ tail.js
@@ -1,22 +1,12 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  let compare = actual === expected;
-  if (compare) {
-    console.log(`👽👽👽 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
+
+// FUNCTION IMPLEMENTATION
 const tail = function(arr) {
   return arr.slice(1);
 };
 
-//TESTING CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
-assertEqual(tail(words).length, 2);
-assertEqual(tail(words)[0],"Lighthouse");
-assertEqual(tail(words)[1],"Labs");
+module.exports = tail;
+
 
 
